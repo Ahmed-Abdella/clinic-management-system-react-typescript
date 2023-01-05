a83b12c208a4ebde8f6b54f54e472064c3ff63c7
Ahmed-Abdella <abdellaa921@gmail.com>
2023-01-05 20:24:43 +0200

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className=" bg-red-500 text-red-400">
+      <h1 className="text-red-400">Hi There!</h1>
+    </div>
+  );
+}
+
+export default App;
