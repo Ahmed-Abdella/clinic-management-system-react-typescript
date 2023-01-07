9e5d89993d50add389037635983985dae90bbdf0
Ahmed-Abdella <abdellaa921@gmail.com>
2023-01-07 05:19:12 +0200

    style the active navlink

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const NavBar: React.FC = () => {
   return (
-    <ul className="fixed top-0 left-0 bottom-0 text-gray-600 min-h-screen flex flex-col gap-10 bg-white rounded-r-xl py-10 [&>*]:[&>*]:flex [&>*]:[&>*]:px-4 [&>*]:[&>*]:py-1  text-lg">
+    <ul className="fixed top-0 left-0 bottom-0 text-gray-600 min-h-screen flex flex-col gap-10 bg-white rounded-r-xl py-10 [&>*]:[&>*]:flex [&>*]:[&>*]:px-4 [&>*]:[&>*]:py-1 [&>*]:[&>*]:border-r-4 [&>*]:[&>*]:border-transparent   [&>*]:[&>*]:rounded-r-sm w-48  ">
       <li>
         <NavLink className="" to={"/"}>
           dashboard
