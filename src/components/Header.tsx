6cd555e4eec5861e972a238f1c507d9c0daebf9b
Ahmed-Abdella <abdellaa921@gmail.com>
2023-01-10 19:41:35 +0200

    style the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,22 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className=" flex items-center justify-between left-0 py-8 px-12">
-      <div className="text-2xl">LOGO</div>
+    <div className=" flex items-center justify-between left-0 py-6 px-28">
+      <div className="text-2xl font-bold">LOGO</div>
       <div className="">
         <ul className=" flex items-center justify-center   [&>*]:[&>*]:px-4 [&>*]:[&>*]:py-2 [&>*]:[&>*]:rounded-lg    ">
           <li className="">
-            <Link className="mr-2" to="/login">
+            <Link
+              className="mr-2 font-semibold text-sky-600 hover:underline  transition duration-100"
+              to="/login"
+            >
               Login
             </Link>
           </li>
 
           <li className="">
             <Link
-              className="text-white bg-sky-600 hover:bg-sky-700 transition duration-100 "
+              className="text-white font-semibold bg-sky-600 hover:bg-sky-700 transition duration-100 "
               to="/signup"
             >
               Signup
