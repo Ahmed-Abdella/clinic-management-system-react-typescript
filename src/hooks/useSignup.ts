a749604ee1ded334dbd3f5b175dd9b9da3fc62e8
Ahmed-Abdella <abdellaa921@gmail.com>
2023-01-18 18:06:33 +0200

    ignoring firebase

--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { auth } from "../firebase";
+import { auth } from "../../firebase/firebase";
 
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useAuthContext } from "./useAuthContext";
--- src/pages/AllPatients.tsx
+++ src/pages/AllPatients.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from "../../firebase/firebase";
 
 interface patientType {
   name: string;
