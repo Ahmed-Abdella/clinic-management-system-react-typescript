eb600efc550be14bddd644cf0fa56d2ea1c0e117
Ahmed-Abdella <abdellaa921@gmail.com>
2023-01-28 07:16:48 +0200

    gender radio input and  add medicinal spices

--- src/pages/AddPatient.tsx
+++ src/pages/AddPatient.tsx
@@ -1,18 +1,40 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const AddPatient: React.FC = () => {
   const [patientName, setPatientName] = useState<string>("");
   const [patientAge, setPatientAge] = useState<number | string>("");
+  const [gender, setGender] = useState<string>("");
   const [diagnosis, setDiagnosis] = useState<string>("");
+
+  const [newSpice, setNewSpice] = useState<string>("");
+  const [spices, setSpices] = useState<string[]>([]);
+
+  // const onGenderChange = (e:React.FormEvent<HTMLFormElement>) => {
+
+  // }
+
+  const spicesInput = useRef<HTMLInputElement | null>(null);
+
+  const handleAdd = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    const spice = newSpice.trim();
+    if (spice && !spices.includes(spice)) {
+      setSpices((prevspices) => [...prevspices, spice]);
+    }
+
+    setNewSpice("");
+
+    spicesInput.current?.focus();
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
 
-    console.log(patientName, patientAge, diagnosis);
+    console.log(patientName, patientAge, diagnosis, gender, spices);
   };
   return (
     <form
-      className="flex flex-col w-full px-36 pb-6 [&_label]:mt-6 [&_label]:flex [&_label]:flex-col [&_input]:mt-1 [&_input]:p-2 [&_input]:border [&_input]:border-gray-600 [&_input]:shadow [&_input]:rounded [&_input]:outline-sky-600 "
+      className="flex flex-col w-full px-36 pb-6 [&_label]:mt-6 [&>label]:flex [&>label]:flex-col [&_input]:mt-1 [&_input]:p-2 [&_input]:border [&_input]:border-gray-600  [&_input]:rounded [&_input]:outline-sky-600 "
       onSubmit={handleSubmit}
     >
       <label>
@@ -37,6 +59,29 @@ const AddPatient: React.FC = () => {
         ></input>
       </label>
 
+      <div className="mt-2 [&>*]:mr-10 [&_input]:ml-1">
+        <span>Gender:</span>
+        <label className="">
+          <span className="text-center">Male</span>
+          <input
+            type="radio"
+            value="Male"
+            checked={gender === "Male"}
+            onChange={(e) => setGender(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Female</span>
+          <input
+            type="radio"
+            value="Female"
+            checked={gender === "Female"}
+            onChange={(e) => setGender(e.target.value)}
+          />
+        </label>
+      </div>
+
       <label>
         <span>Diagnosis:</span>
 
@@ -46,6 +91,33 @@ const AddPatient: React.FC = () => {
           onChange={(e) => setDiagnosis(e.target.value)}
         ></textarea>
       </label>
+
+      <label>
+        <span>Medicinal spice</span>
+        <div className="spices">
+          <input
+            type="text"
+            onChange={(e) => setNewSpice(e.target.value)}
+            value={newSpice}
+            ref={spicesInput}
+          />
+          <button
+            className="ml-2 self-center  text-white bg-green-500 hover:bg-green-600 transition duration-75 font-semibold px-2 py-2 rounded-lg "
+            onClick={handleAdd}
+          >
+            Add spice
+          </button>
+        </div>
+      </label>
+
+      <p className="mt-2">
+        <span className="mr-4">current spices:</span>
+        {spices.map((i) => (
+          <span className="bg-gray-300 py-1  px-2 rounded mr-1" key={i}>
+            {i},
+          </span>
+        ))}
+      </p>
 
       <button className="mt-8 w-40 self-center bg-sky-600 text-white p-2 rounded-lg hover:bg-sky-700 transition duration-200">
         Add Patient
